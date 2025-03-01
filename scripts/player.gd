08076b86ecaf577fa5939760a201e1a3ee5374a3
Dalton Prokosch <103225424+garthable@users.noreply.github.com>
2025-03-01 13:27:53 -0700

    Tuned a bit

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,46 +3,40 @@ extends CharacterBody2D
 @onready var player = $"."
 @onready var camera = $"Camera2D"
 
-const MAX_SPEED: float = 200.0
+const MAX_SPEED: float = 400.0
 const MIN_SPEED: float = 50.0
 
-const MAX_ANGULAR_VELOCITY: float = -10
-const MIN_ANGULAR_VELOCITY: float = 10
+const MAX_ANGULAR_VELOCITY: float = 5
+const MIN_ANGULAR_VELOCITY: float = -5
 
-const ACCELERATION: float = 1.0
-const DECELERATION: float = -1.0
+const ACCELERATION: float = 40.0
+const DECELERATION: float = -20.0
 
-const ANG_ACCELERATION: float = 1.0
-const ANG_DECELERATION: float = -1.0
+const ANGULAR_VELOCITY_POS: float = 3
+const ANGULAR_VELOCITY_NEG: float = -2
 
-var angular_velocity: float = 0.0
+var time_firing: float = 0.0
 
 var speed: float = 0.0
 var theta: float = 0.0
 
 func go_up(delta: float) -> void:
+	time_firing += delta
+	time_firing = min(10.0, time_firing)
+	
 	speed += DECELERATION*delta
 	speed = clampf(speed, MIN_SPEED, MAX_SPEED)
 	
-	angular_velocity += ANG_ACCELERATION*delta
-	angular_velocity = clampf(
-		angular_velocity, 
-		MAX_ANGULAR_VELOCITY, 
-		MIN_ANGULAR_VELOCITY)
-	
-	theta += angular_velocity*delta
+	theta += ANGULAR_VELOCITY_POS*delta
 	
 func go_down(delta: float) -> void:
+	time_firing -= 4*delta
+	time_firing = max(0, time_firing)
+	
 	speed += ACCELERATION*delta
 	speed = clampf(speed, MIN_SPEED, MAX_SPEED)
 	
-	angular_velocity += ANG_DECELERATION*delta
-	angular_velocity = clampf(
-		angular_velocity, 
-		MAX_ANGULAR_VELOCITY, 
-		MIN_ANGULAR_VELOCITY)
-	
-	theta += angular_velocity*delta
+	theta += ANGULAR_VELOCITY_NEG*delta
 
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("space"):
