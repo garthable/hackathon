018d153320e07507acc38e4f8c5dc690ca0d2b20
Dalton Prokosch <103225424+garthable@users.noreply.github.com>
2025-03-01 21:14:00 -0700

    added turret spawn

--- scripts/spawner.gd
+++ scripts/spawner.gd
@@ -3,17 +3,36 @@ extends Node2D
 @onready var enemies = {
 	"imp": preload("res://scenes/imp.tscn"),
 	"fighter": preload("res://scenes/fighter.tscn"),
-	"deployer": preload("res://scenes/deployer.tscn")
+	"deployer": preload("res://scenes/deployer.tscn"),
+	"turret": preload("res://scenes/turret.tscn")
 }
 
 @onready var player = $"../Player"
 @onready var spawner = $"."
 @onready var parent = $"../"
 
+var turret_spawned = false
+
+func spawn_turret() -> bool:
+	var player_pos = player.position
+	var pos = spawner.position
+	if (player_pos - pos).length() <= 200 or turret_spawned:
+		return false
+	var instance = enemies["turret"].instantiate()
+	var dx = pos.x - player_pos.x
+	var dy = pos.y - player_pos.y
+	var theta = atan2(dy, dx)
+	
+	instance.postiion = pos
+	instance.rotation = 0
+	parent.add_child(instance)
+	turret_spawned = true
+	return true
+
 func spawn(name: String) -> bool:
 	var player_pos = player.position
 	var pos = spawner.position
-	if (player_pos - pos).length() <= 200:
+	if (player_pos - pos).length() <= 200 or turret_spawned:
 		return false
 	var instance = enemies[name].instantiate()
 	var dx = pos.x - player_pos.x
